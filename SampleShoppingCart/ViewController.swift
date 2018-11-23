20cd4a394117054de306399d0406c3d3abfd7bcb
Fernando Martin Ortiz <ortizfernandomartin@gmail.com>
2018-11-23 17:13:19 -0300

    First commit

---
+++ SampleShoppingCart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleShoppingCart
+//
+//  Created by Fernando Ortiz on 23/11/2018.
+//  Copyright © 2018 Fernando Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ SampleShoppingCartTests/SampleShoppingCartTests.swift
@@ -0,0 +1,34 @@
+//
+//  SampleShoppingCartTests.swift
+//  SampleShoppingCartTests
+//
+//  Created by Fernando Ortiz on 23/11/2018.
+//  Copyright © 2018 Fernando Ortiz. All rights reserved.
+//
+
+import XCTest
+@testable import SampleShoppingCart
+
+class SampleShoppingCartTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
