df7336995b859fa3eb87579a2e9f058257073c05
Nicolas <nicolas.bolzan@infinixsoft.com>
2018-12-05 17:48:16 -0300

    products added

--- SampleShoppingCart/App/Products/Cells/ProductTableViewCell.swift
+++ SampleShoppingCart/App/Products/Cells/ProductTableViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ProductTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var cantLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
     }
@@ -24,7 +29,16 @@ class ProductTableViewCell: UITableViewCell {
     }
     
     func configure(product: Product) {
-        
+        productImageView.image = UIImage(named: product.picture ?? "")
+        nameLabel.text = product.name
+        cantLabel.text = product.count.description
+        priceLabel.text = product.price.moneyDescription
     }
     
+}
+
+extension Double {
+    var moneyDescription: String{
+        return String(format: "$ %.2f", self)
+    }
 }
--- SampleShoppingCart/App/Products/ProductsViewModel.swift
+++ SampleShoppingCart/App/Products/ProductsViewModel.swift
@@ -7,8 +7,27 @@
 //
 
 import Foundation
+import CoreData
 
 final class ProductsViewModel {
     
     var products: [Product] = []
+    
+    init() {
+//        let product = Product(context: context)
+//        product.name = "Pitusas"
+//        product.count = 10
+//        product.price = 20
+//        product.picture = "Pitusas"
+//
+//        let product2 = Product(context: context)
+//        product2.name = "Manaos"
+//        product2.count = 5
+//        product2.price = 25
+//        product2.picture = "Manaos"
+//
+//        try? context.save()
+        let request = NSFetchRequest<Product>(entityName: "Product")
+        self.products = (try? context.fetch(request)) ?? []
+    }
 }
